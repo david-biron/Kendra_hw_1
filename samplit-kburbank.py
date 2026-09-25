b6dccf0938c81f960b1afe4e907746d74f9d5c75
david-biron <david.biron@gmail.com>
2026-09-25 11:02:01 -0500

    changed samplit in feature branch

--- samplit-kburbank.py
+++ samplit-kburbank.py
@@ -1,12 +1,11 @@
 import pandas as pd
-import sys
-import random
+import sys, random
 
-assert len(sys.argv)==2
+assert len(sys.argv) == 2
 
 filename = sys.argv[1]
 with open(filename, 'r') as file:
     for line in file:
-        if random.random()<1/100:
+        if random.random()<0.1:
             print(line)
 
