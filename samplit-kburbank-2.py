334406c4ee2cb79742951b9b3899304423089df3
david-biron <david.biron@gmail.com>
2026-09-25 09:47:41 -0500

    added second version of script

---
+++ samplit-kburbank-2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import sys
+import random
+
+assert len(sys.argv)==2
+
+filename = sys.argv[1]
+with open(filename, 'r') as file:
+    for line in file:
+        if random.random()<0.01:
+            print(line)
+
