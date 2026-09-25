c9590ee4903225944f20db113d5d83dde41dbc8e
david-biron <david.biron@gmail.com>
2026-09-25 10:37:30 -0500

    changed second script in feature branch

---
+++ samplit-kburbank-2.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import sys, random
+
+assert len(sys.argv)== 2
+
+filename =sys.argv[1]
+with open(filename, 'r') as file:
+    for line in file:
+        if random.random()< 0.0100:
+            print(line)
+
