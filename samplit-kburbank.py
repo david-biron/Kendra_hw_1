b4b699c53283f9197ff1b8bb5d32d6a47e1022e0
david-biron <david.biron@gmail.com>
2026-09-25 10:12:11 -0500

    restrat

--- samplit-kburbank.py
+++ samplit-kburbank.py
@@ -1,11 +1,12 @@
 import pandas as pd
-import sys, random
+import sys
+import random
 
-assert len(sys.argv) == 2
+assert len(sys.argv)==2
 
 filename = sys.argv[1]
 with open(filename, 'r') as file:
     for line in file:
-        if random.random() < 0.01:
+        if random.random()<0.01:
             print(line)
 
